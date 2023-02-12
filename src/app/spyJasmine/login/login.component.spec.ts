7bf643cca4ff191811500a8b14ec3be1ad6820c2
Ahmed Rebai <ahmed.bouhmid94@gmail.com>
2023-02-12 10:24:10 +0100

    spyOn unit test example 2

--- src/app/spyJasmine/login/login.component.spec.ts
+++ src/app/spyJasmine/login/login.component.spec.ts
@@ -1,23 +1,77 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { AuthService } from '../auth.service';
 
 import { LoginComponent } from './login.component';
 
+
+class MockAuthService {
+  authenticated = false;
+
+  isAuthenticated() {
+    return this.authenticated;
+  }
+}
+
 fdescribe('LoginComponent', () => {
   let component: LoginComponent;
+  let mockService: MockAuthService;
+  let service: AuthService;
   let fixture: ComponentFixture<LoginComponent>;
+  let debugElement: DebugElement;
+  let spy: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ LoginComponent ]
     })
     .compileComponents();
-
+    mockService = new MockAuthService();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    debugElement = fixture.debugElement;
+    component = fixture.componentInstance;
+   // service = debugElement.injector.get(AuthService);
+    service = TestBed.inject(AuthService);
+    //spy = spyOn(service, 'isAuthenticated').and.callThrough();
+   // let secondSpy=  spyOn(service, 'isAuthenticated').and.callThrough();
+   fixture.detectChanges();
   });
+
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  xit('needsLogin returns true when the user has not been authenticated', () => {
+    mockService.authenticated = false;
+    expect(component.needsLogin()).toBeTruthy();
+  });
+
+
+  fit('needsLogin returns true when the user has not been authenticated (spy version)', () => {
+    spy = spyOn(service, 'isAuthenticated').and.callThrough();
+    debugElement
+    .query(By.css('a'))
+    .triggerEventHandler('click', null);
+    fixture.detectChanges();
+    expect(component.needsLogin()).toBeTruthy();
+    expect(spy).toHaveBeenCalled();
+  });
+
+
+
+  xit('needsLogin returns true when the user has not been authenticated (jasmine.createSpyObject)', () => {
+    const jasmineSpy = jasmine.createSpyObj('AuthService', ['isAuthenticated']);;
+     expect(component.needsLogin()).toBeTruthy();
+     expect(jasmineSpy.isAuthenticated).toHaveBeenCalledTimes(1);
+   });
+
+  xit('needsLogin returns false when the user has been authenticated', () => {
+    mockService.authenticated = true;
+    expect(component.needsLogin()).toBeFalsy();
   });
 });
